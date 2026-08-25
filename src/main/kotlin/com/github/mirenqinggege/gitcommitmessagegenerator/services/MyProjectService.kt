0d9288428bdd34343717df9894c65d9c7c72a8fd
GitHub Action <action@github.com>
2026-08-25 03:28:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/mirenqinggege/gitcommitmessagegenerator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mirenqinggege/gitcommitmessagegenerator/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.mirenqinggege.gitcommitmessagegenerator.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mirenqinggege.gitcommitmessagegenerator.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
